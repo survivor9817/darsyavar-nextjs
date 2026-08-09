256a7b9921e3c2f2e25cb15781003c331e7116fd
Reza <rezaghezelseflou@gmail.com>
2026-08-09 17:30:51 +0330

    shadcn ui added and other data and some folder structure made beeeeeeaaautifully

---
+++ app/study/[bookId]/[page]/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+type Props = {
+  params: Promise<{
+    bookId: string;
+  }>;
+};
+
+const page = async ({ params }: Props) => {
+  const { bookId } = await params;
+  return (
+    <div>
+      <div>page</div>
+      <div>{bookId}</div>
+    </div>
+  );
+};
+
+export default page;
